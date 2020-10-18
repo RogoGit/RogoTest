4c74cbe6bd69f1ebb0fcc359a8ed2d3f53b53461
Nikita Rogalenko <nikitalr@mail.ru>
2020-10-19 01:47:40 +0300

    lab2: equation system testing done

--- SoftwareTestingLab2/src/test/java/EquationSystemTest.java
+++ SoftwareTestingLab2/src/test/java/EquationSystemTest.java
@@ -12,34 +12,35 @@ import org.mockito.MockitoAnnotations;
 
 class EquationSystemTest {
 
+    @Mock
+    private AdvancedLogFunctions advLogFuncMock = new AdvancedLogFunctions();
+
+    @Mock
+    private AdvancedTrigFunctions advTrigFuncMock = new AdvancedTrigFunctions();
+
+    @BeforeEach
+    void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    private void mockTrig(Double x, Double precision, Double sin, Double cos, Double tan, Double cot, Double sec) {
+        Mockito.when(advTrigFuncMock.sin(x, precision)).thenReturn(sin);
+        Mockito.when(advTrigFuncMock.cos(x, precision)).thenReturn(cos);
+        Mockito.when(advTrigFuncMock.tan(x, precision)).thenReturn(tan);
+        Mockito.when(advTrigFuncMock.cot(x, precision)).thenReturn(cot);
+        Mockito.when(advTrigFuncMock.sec(x, precision)).thenReturn(sec);
+    }
+
+    private void mockLog(Double x, Double precision, Double ln, Double log2, Double log3, Double log5) {
+        Mockito.when(advLogFuncMock.ln(x, precision)).thenReturn(ln);
+        Mockito.when(advLogFuncMock.log_2(x, precision)).thenReturn(log2);
+        Mockito.when(advLogFuncMock.log_3(x, precision)).thenReturn(log3);
+        Mockito.when(advLogFuncMock.log_5(x, precision)).thenReturn(log5);
+    }
+
     @Nested
     class EquationSystemUnitTest {
 
-        @Mock
-        private AdvancedLogFunctions advLogFuncMock = new AdvancedLogFunctions();
-
-        @Mock
-        private AdvancedTrigFunctions advTrigFuncMock = new AdvancedTrigFunctions();
-
-        @BeforeEach
-        void initMocks() {
-            MockitoAnnotations.initMocks(this);
-        }
-
-        private void mockTrig(Double x, Double precision, Double sin, Double cos, Double tan, Double cot, Double sec) {
-            Mockito.when(advTrigFuncMock.sin(x, precision)).thenReturn(sin);
-            Mockito.when(advTrigFuncMock.cos(x, precision)).thenReturn(cos);
-            Mockito.when(advTrigFuncMock.tan(x, precision)).thenReturn(tan);
-            Mockito.when(advTrigFuncMock.cot(x, precision)).thenReturn(cot);
-            Mockito.when(advTrigFuncMock.sec(x, precision)).thenReturn(sec);
-        }
-
-        private void mockLog(Double x, Double precision, Double ln, Double log2, Double log3, Double log5) {
-            Mockito.when(advLogFuncMock.ln(x, precision)).thenReturn(ln);
-            Mockito.when(advLogFuncMock.log_2(x, precision)).thenReturn(log2);
-            Mockito.when(advLogFuncMock.log_3(x, precision)).thenReturn(log3);
-            Mockito.when(advLogFuncMock.log_5(x, precision)).thenReturn(log5);
-        }
 
         @ParameterizedTest
         @CsvFileSource(resources = "/equation_system_test.csv", numLinesToSkip = 1)
@@ -63,7 +64,7 @@ class EquationSystemTest {
 
         @ParameterizedTest
         @CsvFileSource(resources = "/equation_system_test.csv", numLinesToSkip = 1)
-        void testSystemIntegration(Double x, Double precision, Double expected,
+        void testSystemFullIntegration(Double x, Double precision, Double expected,
                             Double sin, Double cos, Double tan, Double cot, Double sec,
                             Double ln, Double log2, Double log3, Double log5, String msg) {
 
@@ -73,6 +74,37 @@ class EquationSystemTest {
 
             Assert.assertEquals("Description: " + msg, expected, actual, precision*10);
         }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/equation_system_test.csv", numLinesToSkip = 1)
+        void testSystemLogIntegration(Double x, Double precision, Double expected,
+                                   Double sin, Double cos, Double tan, Double cot, Double sec,
+                                   Double ln, Double log2, Double log3, Double log5, String msg) {
+
+
+            mockTrig(x, precision, sin, cos, tan, cot, sec);
+            EquationSystem system = new EquationSystem(advTrigFuncMock, new AdvancedLogFunctions());
+
+            Double actual = system.computeFunction(x, precision);
+
+            Assert.assertEquals("Description: " + msg, expected, actual, precision*10);
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/equation_system_test.csv", numLinesToSkip = 1)
+        void testSystemTrigIntegration(Double x, Double precision, Double expected,
+                                       Double sin, Double cos, Double tan, Double cot, Double sec,
+                                       Double ln, Double log2, Double log3, Double log5, String msg) {
+
+
+            mockLog(x, precision, ln, log2, log3, log5);
+            EquationSystem system = new EquationSystem(new AdvancedTrigFunctions(), advLogFuncMock);
+
+            Double actual = system.computeFunction(x, precision);
+
+            Assert.assertEquals("Description: " + msg, expected, actual, precision*10);
+        }
+
 
     }
 
